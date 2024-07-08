1abae01cf52f6e454223755c38cb97beea0f5670
Keith O'Donoghue <amky2syl@students.codeinstitute.net>
2024-07-08 14:40:26 +0000

    Add code for selectRandomCard and dealCards functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
+
+
 let deck = [];
 
 function createDeck() {
-const suits = ["C", "D", "H", "S"];
+const suits = ["♣︎", "♦︎", "♥︎", "♠︎"];
 const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-
 for(let x = 0; x < suits.length; x++) {
     for(let y = 0; y < values.length; y++) {
-        deck.push(values[y] + "-" + suits[x]);
+        deck.push(values[y] + suits[x]);
     }
 }
 return deck;
@@ -19,11 +20,22 @@ for(let i = 0; i < deck.length; i++) {
 }
 }
 
-let deckShuffled = createDeck();
-shuffleDeck(deckShuffled);
-console.log(deckShuffled);
+let deck1 = createDeck();
+shuffleDeck(deck1);
 
+function selectRandomCard() {
+    let randomIndex = Math.floor(Math.random() * deck.length);
+    let card = deck1[randomIndex];
+    deck1.splice(randomIndex, 1);
+    return card;
+}
 
+function dealCards() {
+    let playerHand = [selectRandomCard(), selectRandomCard()];
+    let dealerHand = [selectRandomCard(), selectRandomCard()];
+    console.log(playerHand);
+    console.log(dealerHand);
+}
 
-
-
+dealCards();
+console.log(deck1);
