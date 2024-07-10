11d4e6d4b9124c2e54e15d7492b84d0215070f42
Keith O'Donoghue <amky2syl@students.codeinstitute.net>
2024-07-10 03:21:43 +0000

    Add object to access card values, add code to calculate and display player total

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,12 @@ let deck = [];
 let playerHand = [];
 let dealerHand = [];
 let card;
+let playerTotal = 0;
 
-let standButton = document.getElementById("stand-button");
 
+// Create a deck of cards
 function createDeck() {
-  const suits = ["clubs", "diamonds", "hearts", "spades"];
+  const suits = ["c", "d", "h", "s"]; // "clubs", "diamonds", "hearts", "spades"
   const values = [
     "2",
     "3",
@@ -75,24 +76,125 @@ function cardImages() {
 }
 cardImages();
 
-// Hit/Stand buttons
-
-
+// Hit/Stand Buttons
 
 function hit() {
 
-  playerHand = [selectRandomCard()];
-  
-  console.log(playerHand);
+  let newCard = selectRandomCard();
+  playerHand.push(newCard);
 
-  for (i = 0; i < playerHand.length; i++) {
-    let cardValue = playerHand[i];
-    let imageUrl = "assets/images/cards/" + cardValue + ".png";
-    document.getElementById(
-      "player-cards"
-    ).innerHTML += `<img src="${imageUrl}" alt="${cardValue}">`;
-  }
+  let cardValue = newCard;
+  let imageUrl = "assets/images/cards/" + cardValue + ".png";
+  document.getElementById(
+    "player-cards"
+  ).innerHTML += `<img src="${imageUrl}" alt="${cardValue}">`;
+
+  playerTotal += cardValues[newCard];
+
+  console.log(playerHand);
+  console.log(playerTotal);
 }
 
 let hitButton = document.getElementById("hit-button");
 hitButton.addEventListener("click", hit);
+
+function stand() {
+
+}
+
+let standButton = document.getElementById("stand-button");
+
+
+// Calculate playerTotal
+let cardValues = {
+  "2c": 2,
+  "2d": 2,
+  "2h": 2,
+  "2s": 2,
+  "3c": 3,
+  "3d": 3,
+  "3h": 3,
+  "3s": 3,
+  "4c": 4,
+  "4d": 4,
+  "4h": 4,
+  "4s": 4,
+  "5c": 5,
+  "5d": 5,
+  "5h": 5,
+  "5s": 5,
+  "6c": 6,
+  "6d": 6,
+  "6h": 6,
+  "6s": 6,
+  "7c": 7,
+  "7d": 7,
+  "7h": 7,
+  "7s": 7,
+  "8c": 8,
+  "8d": 8,
+  "8h": 8,
+  "8s": 8,
+  "9c": 9,
+  "9d": 9,
+  "9h": 9,
+  "9s": 9,
+  "10c": 10,
+  "10d": 10,
+  "10h": 10,
+  "10s": 10,
+  "Jc": 10,
+  "Jd": 10,
+  "Jh": 10,
+  "Js": 10,
+  "Qc": 10,
+  "Qd": 10,
+  "Qh": 10,
+  "Qs": 10,
+  "Kc": 10,
+  "Kd": 10,
+  "Kh": 10,
+  "Ks": 10,
+  "Ac": 11,
+  "Ad": 11,
+  "Ah": 11,
+  "As": 11
+}
+
+let playerCardValue = cardValues[playerHand[0]] + cardValues[playerHand[1]];
+playerTotal += playerCardValue;
+
+console.log(playerTotal);
+
+
+
+
+// function cardValue(deck1) {
+//   for(card in playerHand) {
+//     let value = card.splice(0, -1);
+//     if(value === "A") {
+//       return playerTotal + 11 <= 21 ? 11 : 1;
+//     } else if(["K", "Q", "J"].includes(value)) {
+//       return 10;
+//       } else {
+//         return parseInt(value)
+//       } 
+//   }
+//   cardValue();
+
+
+//   // const rank = card.slice(0, -1); // Remove the suit from the card value
+//   // if (rank === "A") {
+//   //    // Handle Ace (choose 1 or 11 based on total)
+//   //  return playerTotal + 11 <= 21 ? 11 : 1;
+//   // } else if (["K", "Q", "J"].includes(rank)) {
+//   //  return 10; // Face cards have a value of 10
+//   //   } else {
+//   //   return parseInt(rank); // Numeric cards have their face value
+//   //   }
+// }
+
+// New Game Button
+
+// let newGame = document.getElementById("new-game-button");
+// newGame.addEventListener("click", dealCards);
